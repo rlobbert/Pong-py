ef9efb49da281243e3d619fe26573b68abb04efd
rlobbert <rlobbert27@gmail.com>
2019-12-07 18:08:54 +0000

    Added Paddle movement + collision and Ball

--- Pong.py
+++ Pong.py
@@ -1,13 +1,11 @@
-# Pong 0.1
 # A Pong clone built using Pygame
-
 import pygame
 
 pygame.init()
 
 # Game window settings
 window = pygame.display.set_mode((600, 400))
-pygame.display.set_caption("Pong - version 0.1")
+pygame.display.set_caption("Pong")
 window.fill((0, 0, 0))
 clock = pygame.time.Clock()
 
@@ -20,9 +18,23 @@ class lines(object):
         self.y = y
         self.width = width
         self.height = height
+        self.velocity = 20
 
     def draw(self, window):
         pygame.draw.rect(window, (255, 255, 255), (self.x, self.y, self.width, self.height))
+
+    def move_paddle(self, window):
+        #Player 1 Move and Collision
+        if keys[pygame.K_w] and paddle1.y > (top_wall.y + top_wall.height):
+            paddle1.y -= paddle1.velocity
+        if keys[pygame.K_s] and (paddle1.y + paddle1.height) < (bottom_wall.y - bottom_wall.height):
+            paddle1.y += paddle1.velocity
+
+        #Player 2 Move and Collision
+        if keys[pygame.K_i] and paddle2.y > (top_wall.y + top_wall.height):
+            paddle2.y -= paddle2.velocity
+        if keys[pygame.K_k] and (paddle2.y + paddle2.height) < (bottom_wall.y - bottom_wall.height):
+            paddle2.y += paddle2.velocity
 
 
 # Half Court Circle and Ball
@@ -46,6 +58,8 @@ def redraw_game_window():
     half_court_circle.draw(window)
     paddle1.draw(window)
     paddle2.draw(window)
+    paddle1.move_paddle(window)
+    ball.draw(window)
     pygame.display.update()
 
 
@@ -58,6 +72,7 @@ half_court = lines(300, 35, 3, 330)
 half_court_circle = circles(300, 205, 70, 1)
 paddle1 = lines(30, 170, 10, 75)
 paddle2 = lines(565,170,10, 75)
+ball = circles(200, 200, 7, 7)
 
 
 # Main Game
@@ -65,12 +80,12 @@ paddle2 = lines(565,170,10, 75)
 while run:
     # Main game Variables
     pygame.time.delay(100)
+    keys = pygame.key.get_pressed()
     redraw_game_window()
 
     # Quit event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
 
 pygame.quit()
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Pong-py
-Classic Pong game developed in Python using Pygame
+Creating the classic Pong game in Python using Pygame as i am learning how to
+code.
+
+
+# Requirements, installation and controls
+Requirements:
+  Python 3.8
+  Pygame 1.9.6
+
+Installation:
+  Download the repository or clone it
+  Run Pong.py
+
+  Controls:
+    Player 1:
+      w > up
+      s > down
+
+    Player 2:
+      i > up
+      k > down
