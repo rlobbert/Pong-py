5d118f303607cd1fd4fa740ef28963d0f0ba7288
rlobbert <rlobbert27@gmail.com>
2019-12-15 17:46:22 +0000

    Minor Changes

--- Pong.py
+++ Pong.py
@@ -44,8 +44,8 @@ class ball(object):
         self.y = y
         self.width = width
         self.height = height
-        self.dx = -10#random.choice(ball_movement)
-        self.dy = -10#random.choice(ball_movement)
+        self.dx = random.choice(ball_movement)
+        self.dy = random.choice(ball_movement)
 
     def draw(self, window):
         pygame.draw.rect(window, (255, 255, 255), (self.x, self.y, self.width, self.height))
@@ -53,6 +53,24 @@ class ball(object):
     def move(self, window):
         self.x += self.dx
         self.y += self.dy
+
+    def collision(self, window):
+        # Player 1
+        if ball.x < 40 and (ball.y > paddle1.y - 30 and ball.y < paddle1.y + 75):
+            ball.dx *= -1
+
+        # Player 2
+        if (ball.x < 565 and ball.x > 550) and (ball.y > paddle2.y - 30 and ball.y < paddle2.y + 75):
+            ball.dx *= -1
+
+        # Collision with the Walls
+        # Top Wall
+        if ball.y <= (top_wall.y + top_wall.height):
+            ball.dy *= -1
+
+        # Bottom Wall
+        if ball.y >= (bottom_wall.y - bottom_wall.height):
+            ball.dy *= -1
 
 # Walls and Lines
 class lines(object):
@@ -94,11 +112,14 @@ def redraw_game_window():
     paddle1.move_paddle(window)
     ball.draw(window)
     ball.move(window)
+    ball.collision(window)
     pygame.display.update()
+
 
 ############### Variables ###############
 run = True
 font = pygame.font.SysFont('comicsans', 30)
+large_font = pygame.font.SysFont('comicsans', 118)
 top_wall = lines(0, 35, 600, 15)
 bottom_wall = lines(0, 365, 600, 15)
 half_court = lines(300, 35, 3, 330)
@@ -106,7 +127,7 @@ half_court_circle = circles(300, 205, 70, 1)
 paddle1 = player(30, 170, 10, 75)
 paddle2 = player(565,170,10, 75)
 ball_movement = [-10, 10]
-ball = ball(350, 250, 15, 15)
+ball = ball(300, 200, 15, 15)
 p1_score = 0
 p2_score = 0
 
@@ -124,7 +145,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    # Scoring System
+   #Scoring
     # Player 1
     if ball.x > 590:
         ball.x = 350
@@ -138,23 +159,6 @@ while run:
         ball.y = 250
         ball.dx *= -1
         p2_score += 1
-
-    # Collision with the Paddles
-    # Player 1
-    if ball.x < 40 and (ball.y > paddle1.y - 30 and ball.y < paddle1.y + 75):
-        ball.dx *= -1
-
-    # Player 2
-    if (ball.x < 565 and ball.x > 550) and (ball.y > paddle2.y - 30 and ball.y < paddle2.y + 75):
-        ball.dx *= -1
-
-    # Collision with the Walls
-    # Top Wall
-    if ball.y <= (top_wall.y + top_wall.height):
-        ball.dy *= -1
-
-    # Bottom Wall
-    if ball.y >= (bottom_wall.y - bottom_wall.height):
-        ball.dy *= -1
+    
 
 pygame.quit()
